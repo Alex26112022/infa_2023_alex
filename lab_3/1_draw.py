3199feab43640115eb4888c652edb82a008b9bd1
ElvisPresly <elvis.presly.039127@gmail.com>
2023-05-03 23:06:11 +0400

    Рисуем 1-ю картинку в pygame. Каркас

---
+++ lab_3/1_draw.py
@@ -0,0 +1,26 @@
+import sys
+
+import pygame
+from pygame.draw import *
+
+
+def main():
+    pygame.init()
+    fps = 30
+    screen = pygame.display.set_mode((800, 600))
+
+    def png_1():
+        pass
+
+    pygame.display.update()
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+if __name__ == '__main__':
+    main()
