aab7e3f9b49f8a82c7959e0d6ee0d77cccefaf66
ElvisPresly <elvis.presly.039127@gmail.com>
2023-05-07 16:51:20 +0400

    Используем функцию pygame.key.get_pressed().

--- lab_3/probnaya_igra.py
+++ lab_3/probnaya_igra.py
@@ -8,21 +8,15 @@ h = 600  # Высота экрана
 fps = 60
 screen = pygame.display.set_mode((w, h))
 
-white = (255, 255, 255)
-black = (0, 0, 0)
-gray = (125, 125, 125)
-light_blue = (64, 128, 255)
-green = (0, 200, 64)
-yellow = (225, 225, 0)
-pink = (230, 50, 230)
-red = (255, 0, 0)
+white = [255, 255, 255]
+black = [0, 0, 0]
+gray = [125, 125, 125]
+light_blue = [64, 128, 255]
+green = [0, 200, 64]
+yellow = [225, 225, 0]
+pink = [230, 50, 230]
+red = [255, 0, 0]
 
-right = "to the right"
-left = "to the left"
-up = "to the up"
-down = "to the down"
-stop = "stop"
-motion = stop
 # Координаты и радиус круга
 x = w // 2
 y = h // 2
@@ -35,30 +29,20 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                motion = left
-            elif event.key == pygame.K_RIGHT:
-                motion = right
-            elif event.key == pygame.K_UP:
-                motion = up
-            elif event.key == pygame.K_DOWN:
-                motion = down
-        elif event.type == pygame.KEYUP:
-            if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
-                motion = stop
 
-    screen.fill([125, 125, 125])
-    pygame.draw.circle(screen, green, (x, y), r)
-    pygame.display.update()
+    keys = pygame.key.get_pressed()
 
-    if motion == left:
+    if keys[pygame.K_LEFT]:
         x -= 3
-    elif motion == right:
+    elif keys[pygame.K_RIGHT]:
         x += 3
-    elif motion == up:
+    elif keys[pygame.K_UP]:
         y -= 3
-    elif motion == down:
+    elif keys[pygame.K_DOWN]:
         y += 3
+
+    screen.fill(light_blue)
+    pygame.draw.circle(screen, red, (x, y), r)
+    pygame.display.update()
 
     clock.tick(fps)
