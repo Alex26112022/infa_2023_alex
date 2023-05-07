01731f80a834b6aadc6519c52ccfa73aec8334d5
ElvisPresly <elvis.presly.039127@gmail.com>
2023-05-07 16:42:45 +0400

    Двигается при длительном нажатии клавиши

--- lab_3/probnaya_igra.py
+++ lab_3/probnaya_igra.py
@@ -17,12 +17,19 @@ yellow = (225, 225, 0)
 pink = (230, 50, 230)
 red = (255, 0, 0)
 
+right = "to the right"
+left = "to the left"
+up = "to the up"
+down = "to the down"
+stop = "stop"
+motion = stop
 # Координаты и радиус круга
 x = w // 2
 y = h // 2
 r = 50
 
 clock = pygame.time.Clock()
+
 while True:
 
     for event in pygame.event.get():
@@ -30,14 +37,28 @@ while True:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x -= 3
+                motion = left
             elif event.key == pygame.K_RIGHT:
-                x += 3
+                motion = right
             elif event.key == pygame.K_UP:
-                y -= 3
+                motion = up
             elif event.key == pygame.K_DOWN:
-                y += 3
+                motion = down
+        elif event.type == pygame.KEYUP:
+            if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
+                motion = stop
+
     screen.fill([125, 125, 125])
     pygame.draw.circle(screen, green, (x, y), r)
     pygame.display.update()
+
+    if motion == left:
+        x -= 3
+    elif motion == right:
+        x += 3
+    elif motion == up:
+        y -= 3
+    elif motion == down:
+        y += 3
+
     clock.tick(fps)
